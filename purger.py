49fbc0235290fd0d88bd46035bbd2d3e081e8962
Mert Bora İnevi <mertborainevi@gmail.com>
2025-01-16 20:13:52 +0300

    use creation date

--- purger.py
+++ purger.py
@@ -6,7 +6,7 @@ def scan_purge_zones(purgelist):
     for uuid in zone_data:
         zone = zone_data[uuid]
 
-        if zone["owner"] is None and not zone["private"] and not zone["has_protected_block"] and (t - zone["last_active_date"]).days >= 1:
+        if zone["owner"] is None and not zone["private"] and not zone["has_protected_block"] and (t - zone["creation_date"]).days >= 1:
             purgelist.append(uuid)
 
 def scan_purge_zones_except_short_name_plain(purgelist):
@@ -14,5 +14,5 @@ def scan_purge_zones_except_short_name_plain(purgelist):
     for uuid in zone_data:
         zone = zone_data[uuid]
 
-        if zone["owner"] is None and not zone["private"] and not zone["has_protected_block"] and (t - zone["last_active_date"]).days >= 1 and (" " in zone["name"] or not zone["biome"] == "plain"):
+        if zone["owner"] is None and not zone["private"] and not zone["has_protected_block"] and (t - zone["creation_date"]).days >= 1 and (" " in zone["name"] or not zone["biome"] == "plain"):
             purgelist.append(uuid)
